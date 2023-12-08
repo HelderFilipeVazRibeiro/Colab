b09f06d0dd95c4197a2c8ca39d2b8365c5224109
HelderFilipeVazRibeiro <helder.ribeiro@orangedynamic.pt>
2023-12-08 12:04:42 +0000

    Update perimetro.py

--- files py/perimetro.py
+++ files py/perimetro.py
@@ -3,7 +3,7 @@ def perimetro():
     print("Defina o comprimento e largura para definir o perímetro du uma determinada área.\nCasas decimais devem ser representadas com .:")
     comp = float(input("Defina o comprimento em metros: "))
     larg = float(input("Defina o comprimento em metros: "))
-    perim = comp * larg
+    perim = 2 * (comp + larg)
     print( f"A soma entre o comprimento {comp:.2f}m e a largura {larg:.2f}m dá o perímetro de {perim:.2f}m²")
   except ValueError:
     print("Por favor, certifique-se de que introduziu os valores corretos. Caracteres especiais, texto e virgulas não são reconhecidos.")
